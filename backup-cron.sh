d54e61056bb9d90683df429df6474052fd879357
Nelson Elhage <nelhage@nelhage.com>
2012-01-22 13:54:32 -0500

    Adjust the logic on the backup-cron script.

--- backup-cron.sh
+++ backup-cron.sh
@@ -5,14 +5,27 @@
         exit 1;
     fi
 
+    ip_dst=$(dig +short nelhage.com)
+
+    [ "$ip_dst" ] || exit 0
+
+    route=$(ip r get "$ip_dst")
+
     PATH=/usr/local/bin:/usr/bin:/bin
     ARGS=""
-    if ! /sbin/ifconfig | grep -qF addr:18. ||
-        /sbin/ifconfig | grep -qF addr:18.100 ||
-        /sbin/ifconfig | grep -qF addr:18.101;
-    then
-        ARGS="-t 200"
-    fi
+
+    tethering() {
+        ip link ls usb0 >/dev/null 2>&1
+    }
+
+    tethering && exit 0;
+
+#    if ! ( echo $route | grep -qF 'via 18.' ||
+#            echo $route | grep -qF 'via 18.100' ||
+#            echo $route | grep -qF 'via 18.101' );
+#    then
+    ARGS="-t 200"
+#    fi
 
     /home/nelhage/bin/backup.sh $ARGS
 ) 9>/home/nelhage/.backup/lock
